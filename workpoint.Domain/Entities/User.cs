462f78bd4e4502bbb9c9893d53ccaae7f0026960
mateocasta33 <162051608+mateocasta33@users.noreply.github.com>
2025-11-27 16:09:44 -0500

    register2/jwt

--- workpoint.Domain/Entities/User.cs
+++ workpoint.Domain/Entities/User.cs
@@ -16,6 +16,7 @@ public class User
     public string? RefreshToken { get; set; }
     public DateTime? RefreshTokenExpire { get; set; }
     
+    
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
     
